d08ce67c9bc4c5c7238c467679de69dda649f7cd
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-07-25 15:32:16 +0100

    Remove Expanded Links Caching
    The means to generate this has been sped up and we are hoping that this
    is cache is no longer required.

--- app/controllers/v2/link_sets_controller.rb
+++ app/controllers/v2/link_sets_controller.rb
@@ -5,13 +5,11 @@ module V2
     end
 
     def expanded_links
-      json = Rails.cache.fetch ['expanded-links', content_id, params[:with_drafts], params[:locale]], expires_in: 1.hour do
-        Queries::GetExpandedLinks.call(
-          content_id,
-          params[:locale],
-          with_drafts: with_drafts?,
-        )
-      end
+      json = Queries::GetExpandedLinks.call(
+        content_id,
+        params[:locale],
+        with_drafts: with_drafts?,
+      )
 
       render json: json
     end
